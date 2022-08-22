03b82c5ebd0608bf8b0114bc8c5aa4dd339282a2
axelGITT <axel_87@o2.pl>
2022-08-23 01:47:18 +0200

    Adding new cars

--- Test/src/main.java
+++ Test/src/main.java
@@ -1,6 +1,16 @@
+import java.util.ArrayList;
 
 public class main {
 	public static void main(String[] args) {
+		
+		ArrayList<String> cars = new ArrayList<>();
+	    cars.add("Volvo");
+	    cars.add("BMW");
+	    cars.add("Ford");
+	    cars.add("Mazda");
+		
+		
+		
 		String sen = "JavaCoding!";
 		System.out.println(sen.length());
 	}
